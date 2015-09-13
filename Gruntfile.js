8e4153114ba1725a8f0442f68822dcd0a824a6a9
kiaoro <kiaoro.com@gmail.com>
2015-09-13 21:20:12 +0200

    misc

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,27 +7,44 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 
+    dirs: {
+      output: 'dist'
+    },
+
 		clean: {
       build: { 
-        src: 'web/built' 
+        src: '<%= dirs.output %>/*' 
       }, 
     },
 
 		less: {
-      dist: {
+      build: {
         options: {
           compress: true,
           optimization: 2
         },
         files: {
-          'web/css/main.css': [
-            'bower_components/bootsrap/dist/less/bootstrap.less',
-            //'src/Naissance/ApplicationBundle/Resources/public/less/main.less'
+          '<%= dirs.output %>/css/built.css': [
+            'bower_components/bootstrap/less/bootstrap.less', 
+            'bower_components/font-awesome/css/font-awesome.css'
+            'src/**/*.css', 
+            'src/**/*.less'
           ]
         }
       }
     },
 
+    cssmin: {
+      options: {
+        report: 'gzip',
+        keepSpecialComments: 0
+      },
+      build: {
+        files: {
+          'web/css/app.min.css': '<%= dirs.output %>/css/built.css'
+        }
+      }
+    }, 
 
     concat: {
       options: {
@@ -35,33 +52,45 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [
-        	'bower_components/bootsrap/dist/jquery.js',
+        	'bower_components/jquery/dist/jquery.js',
         	'bower_components/bootsrap/dist/js/bootstrap.js',
           'bower_components/bootsrap/js/*.js', 
+          'src/**/*.js'
         ], 
-        dest: 'web/built/js/built.js' // la destination finale
+        dest: '<%= dirs.output %>/js/built.js' // la destination finale
       }
     }, 
 
     uglify: {
       options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n', 
         mangle: false,
-        sourceMap: true
+        sourceMap: false
       },
       dist: {
         files: {
-          'web/built/js/built.min.js': [
-            'web/built/js/built.js',
+          'web/js/app.min.js': [
+            '<%= dirs.output %>/js/built.js',
           ]
         }
       }
+    }, 
+
+    watch: {
+      stylesheets: { 
+        files: ['src/**/*.css', 'src/**/*.less'], 
+        tasks: ['css'] 
+      },
+      scripts: { 
+        files: 'src/**/*.js', 
+        tasks: ['javascript'] 
+      } 
     }
-    
 
 	});
 
-	grunt.registerTask('default', ['css', 'javascript']);
-  grunt.registerTask('css', ['less']);
+	grunt.registerTask('default', ['clean', 'css', 'javascript']);
+  grunt.registerTask('css', ['less', 'cssmin']);
   grunt.registerTask('javascript', ['concat', 'uglify']);
 
 }
